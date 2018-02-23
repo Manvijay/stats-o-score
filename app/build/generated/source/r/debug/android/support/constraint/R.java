76b676379f106090806b14156ec4c2a9f18bd30c
vsaharan07 <36014258+vsaharan07@users.noreply.github.com>
2018-02-23 04:21:39 -0500

    Rankings Layout Changes

--- app/build/generated/source/r/debug/android/support/constraint/R.java
+++ app/build/generated/source/r/debug/android/support/constraint/R.java
@@ -54,14 +54,14 @@ public final class R {
         public static final int layout_optimizationLevel = 0x7f0300f2;
     }
     public static final class id {
-        public static final int all = 0x7f08001c;
-        public static final int basic = 0x7f080020;
-        public static final int chains = 0x7f08002b;
-        public static final int none = 0x7f08006a;
-        public static final int packed = 0x7f08006f;
-        public static final int parent = 0x7f080071;
-        public static final int spread = 0x7f08009f;
-        public static final int spread_inside = 0x7f0800a0;
+        public static final int all = 0x7f08002d;
+        public static final int basic = 0x7f080031;
+        public static final int chains = 0x7f08003c;
+        public static final int none = 0x7f08007a;
+        public static final int packed = 0x7f08007f;
+        public static final int parent = 0x7f080081;
+        public static final int spread = 0x7f0800ae;
+        public static final int spread_inside = 0x7f0800af;
         public static final int wrap = 0x7f0800d3;
     }
     public static final class styleable {
